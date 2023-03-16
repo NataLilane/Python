81c8fd1a19423bd1937902e05619dc8a1496f27c
NataLilane <114179495+NataLilane@users.noreply.github.com>
2023-03-16 06:48:32 +0300

    Update creat_bot.py

---
+++ Семинары/Seminar10/creat_bot.py
@@ -0,0 +1,6 @@
+from aiogram import Bot, Dispatcher
+from aiogram.utils import executor
+from aiogram.types import Message
+
+bot = Bot('6145665180:AAEGJ3_yhhgo2cfI_sh8AkQQdSXF2MzhF9k')
+dp = Dispatcher(bot)
